b436b5454297692ba827de44461b5bd6ff4ca588
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-30 16:40:34 +0200

    another attempt to get the previous evolution, had to comment it out for now as it gives a lot of errors

--- index.php
+++ index.php
@@ -4,46 +4,61 @@ declare(strict_types=1);
 
 ini_set('display_errors', '1');
 
-ini_set ('display_startup_errors','1');
+ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 echo "<h2>Welcome to the pokedex</h2>";
 
 //placeholding a pokemon for the moment to test
-$pokemon=$_POST['input'];
-if($pokemon===null){
-    $pokemon=1;
+
+
+$pokemon = $_POST['input'];
+
+if ($pokemon === null) {
+    $pokemon = 1;
 }
+//$pokemonLow = strtolower($pokemon);
 
+$PokemonData = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $pokemon);
 
-$PokemonData = file_get_contents('https://pokeapi.co/api/v2/pokemon/'.$pokemon);
-
-$pokemonQuery=json_decode($PokemonData,true);
+$pokemonQuery = json_decode($PokemonData, true);
 //shows a lot of unfiltered data.
 
 //echo only accepts strings
-$idNum=$pokemonQuery['id'];
-$pokeName=$pokemonQuery['name'];
+$idNum = $pokemonQuery['id'];
+$pokeName = $pokemonQuery['name'];
 
-$movesList= $pokemonQuery['moves'];
+$movesList = $pokemonQuery['moves'];
+
+/*
+$evoData = file_get_contents($pokemonQuery['evolution_chain']['url'], true);
+$evoQuery = json_decode($evoData, true);
+
+$previousForm = $evoQuery['evolves_from_species']['name'];
+$PreviousData = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $previousForm);
+$previousQuery = json_decode($PreviousData, true);
+*/
+/*
+if ($evoQuery['evolves_from_species'] === null) {
+    echo 'no previous evolution';
+} else {
 
 
-
-
-echo $idNum.'  ';
-
-echo $pokeName;
+}
+*/
+echo "<strong>$idNum</strong>";
+echo " ";
+echo "<strong>$pokeName</strong>";
 echo "<br>";
 echo "<br>";
 
 $i = 0;
 for ($i = 0; $i < 4; $i++) {
-    echo  $moves[] = $movesList[$i]['move']['name']."<br />";
+    echo $moves[] = $movesList[$i]['move']['name'] . "<br />";
 
 
 }
-
-
+echo "<br>";
 
 
 ?>
@@ -61,10 +76,11 @@ for ($i = 0; $i < 4; $i++) {
 
 
 <form action="index.php" method="post">
-    <input type="text" name="input" placeholder="Enter pokemon name or id" />
-    <input type="submit" />
+    <input type="text" name="input" placeholder="Enter pokemon name or id"/>
+    <input type="submit"/>
 </form>
-<img src= "<?php echo $pokemonQuery['sprites']['front_default']?>"
+<img src="<?php echo $pokemonQuery['sprites']['front_default'] ?>"
+
 </body>
 </html>
 
